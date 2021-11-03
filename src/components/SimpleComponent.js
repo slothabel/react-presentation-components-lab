024f8e0c47775a2cd3f58aa131698fd18300f749
Yoshi <yoshimejia00@gmail.com>
2021-11-03 15:36:36 -0400

    commit tree 2f26a561e5682afa12a172ad0fd2054540522d08

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
-// Code SimpleComponent Here
+import React from "react";
+
+export default class SimpleComponent extends React.Component {
+    state = {
+        mood: "happy"
+    }
+
+    // did not pass when i had this as handleClick(){}, had to switch to arrow function
+    handleClick = () => {
+        this.setState({
+            mood: "sad"
+        })
+    }
+
+    render(){
+        return(
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
